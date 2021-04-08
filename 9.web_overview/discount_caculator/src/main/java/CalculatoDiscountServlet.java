bface0d22fd138f88787e9b822ce2d0c6eaa59b7
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-04-08 23:37:42 +0700

    up bo sung web overview

--- 9.web_overview/discount_caculator/src/main/java/CalculatoDiscountServlet.java
+++ 9.web_overview/discount_caculator/src/main/java/CalculatoDiscountServlet.java
@@ -2,6 +2,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CalculatoDiscountServlet", urlPatterns = "/display-discount")
 public class CalculatoDiscountServlet extends HttpServlet {
@@ -12,9 +13,19 @@ public class CalculatoDiscountServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            String description = request.getParameter("description");
-            float list_price = Float.parseFloat(request.getParameter("list_price"));
-            float discount_percent = Float.parseFloat(request.getParameter("discount_percent"));
-            float discount_amount = (float) (list_price * discount_percent * 0.01);
+        String description = request.getParameter("description");
+        float list_price = Float.parseFloat(request.getParameter("list_price"));
+        float discount_percent = Float.parseFloat(request.getParameter("discount_percent"));
+
+        float discount_amount = (float) (list_price * discount_percent * 0.01);
+        float discountPrice = list_price - discount_amount;
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("Tên sản phẩm: " + description);
+        writer.println("Giá sản phẩm: " + list_price);
+        writer.println("Giảm giá: " + discount_percent + "%");
+        writer.println("Giá được giảm: " + discount_amount);
+        writer.println("Giá sản phẩm sau khi giảm: " + discountPrice);
+        writer.println("</html>");
     }
 }
